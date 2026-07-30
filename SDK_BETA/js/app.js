7ad3440df577616d284485d1393fcd2830cfe26d
Valentino Aglianó <zero419@gmail.com>
2026-07-30 21:09:14 +0200

    Update app.js

--- SDK_BETA/js/app.js
+++ SDK_BETA/js/app.js
@@ -220,6 +220,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // 🚀 Inizializzazione SDK con Passaggio Parametrico dei Core Assets
     const sdk = new PanzerSDK({
         swPath: './SW.js',
+        scope: '/SDK_BETA',
         coreAssets: getDefinedCoreAssets(),
         
         onLogMessage: (level, msg) => logToTerminal(level, msg),
