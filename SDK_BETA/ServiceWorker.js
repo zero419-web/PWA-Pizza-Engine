63b35050c3830c10be553ff76a5f42b19dda1394
Valentino Aglianó <zero419@gmail.com>
2026-07-30 22:01:47 +0200

    Update ServiceWorker.js

--- SDK_BETA/ServiceWorker.js
+++ SDK_BETA/ServiceWorker.js
@@ -55,7 +55,7 @@ let encryptionKey = null;
 
  let isLogicEnabled = false;
  let syncAbortController = null;
-const BASE_PATH = self.location.pathname.replace(/(.*\/SDK_BETA\/).*/i, "$1").replace(/\/+/g, '/');
+const BASE_PATH = self.location.pathname.replace(/[^\/]+$/, "").replace(/\/+/g, '/');
 
 
 
